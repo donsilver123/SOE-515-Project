7d86c8cfd84954235a65ae224cec790e08e50ba7
Adophilus <uchenna19of@gmail.com>
2025-05-10 14:16:29 +0100

    chore: updated the setup script

--- packages/insurance/contract/script/setup.sh
+++ packages/insurance/contract/script/setup.sh
@@ -4,19 +4,19 @@ cast send \
   --private-key "$PRIVATE_KEY" \
   --rpc-url "$RPC_URL" \
   "$INSURANCE_INSTITUTION_CONTRACT_ADDRESS" \
-  "addInsurancePlan(string,uint,uint[])" 'BASIC' 10000 '[0,1,2,3,4]' \
-  -vvvv
-
-cast send \
-  --private-key "$PRIVATE_KEY" \
-  --rpc-url "$RPC_URL" \
-  "$INSURANCE_INSTITUTION_CONTRACT_ADDRESS" \
-  "addInsurancePlan(string,uint,uint[])" 'INTERMEDIATE' 50000 '[0,1,2,3,4,5,6,7,8,9]' \
-  -vvvv
-
-cast send \
-  --private-key "$PRIVATE_KEY" \
-  --rpc-url "$RPC_URL" \
-  "$INSURANCE_INSTITUTION_CONTRACT_ADDRESS" \
-  "addInsurancePlan(string,uint,uint[])" 'ADVANCED' 100000 '[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]' \
-  -vvvv
+  "addInsurancePlan(string,uint,uint8[])" 'BASIC' 10000 '[0,1,2,3,4]' \
+  -vvvv \
+  && \
+  cast send \
+    --private-key "$PRIVATE_KEY" \
+    --rpc-url "$RPC_URL" \
+    "$INSURANCE_INSTITUTION_CONTRACT_ADDRESS" \
+    "addInsurancePlan(string,uint,uint8[])" 'INTERMEDIATE' 50000 '[0,1,2,3,4,5,6,7,8,9]' \
+    -vvvv \
+    && \
+    cast send \
+      --private-key "$PRIVATE_KEY" \
+      --rpc-url "$RPC_URL" \
+      "$INSURANCE_INSTITUTION_CONTRACT_ADDRESS" \
+      "addInsurancePlan(string,uint,uint8[])" 'ADVANCED' 100000 '[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]' \
+      -vvvv
